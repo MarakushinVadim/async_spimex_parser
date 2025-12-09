ad7025503d196e528959c6eeec1119717d9022c7
Vadim <vadim.marakushin@gmail.com>
2025-12-09 08:54:59 +0300

    1

--- config.py
+++ config.py
@@ -1,19 +1,25 @@
+import os
 import re
 
 from sqlalchemy import URL
+import dotenv
 
-LINKS_PATTERN: re.Pattern[str] = re.compile(r"^/files/trades/result/upload/reports/oil_xls/oil_xls_202([543]).*")
+dotenv.load_dotenv()
+
+LINKS_PATTERN: re.Pattern[str] = re.compile(
+    r"^/files/trades/result/upload/reports/oil_xls/oil_xls_202([543]).*"
+)
+
 
 class DBConfig:
-    USER_NAME = '****'  # Введите имя пользователя постгрес
-    PASSWORD = "*****" # Введите пароль пользователя постгрес
-    DATABASE_NAME = 'spimex_trading_results'  # Введите название базы данных
+    USER_NAME = os.getenv("USER_NAME")  # Введите имя пользователя постгрес
+    PASSWORD = os.getenv("PASSWORD")  # Введите пароль пользователя постгрес
+    DATABASE_NAME = os.getenv("DATABASE_NAME")  # Введите название базы данных
     DB_URL = URL.create(
         drivername="postgresql+asyncpg",
         username=USER_NAME,
         password=PASSWORD,
         host="localhost",
         port=5432,
-        database=DATABASE_NAME
+        database=DATABASE_NAME,
     )
-
--- db.py
+++ db.py
@@ -1,32 +1,39 @@
+import logging
+
 from models import Base, SpimexTradingResultsBase
 
 
-async def init_db(engine):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+class DataBase:
+    def __init__(self, engine):
+        self.engine = engine
 
-async def add_data(session, data):
-    async with session() as session:
-        async with session.begin():
-            try:
-                obj_ = SpimexTradingResultsBase(
-                    exchange_product_id=data.exchange_product_id,
-                    exchange_product_name=data.exchange_product_name,
-                    oil_id=data.oil_id,
-                    delivery_basis_id=data.delivery_basis_id,
-                    delivery_basis_name=data.delivery_basis_name,
-                    delivery_type_id=data.delivery_type_id,
-                    volume=data.volume,
-                    total=data.total,
-                    count=data.count,
-                    date=data.date,
-                    created_on=data.created_on,
-                    updated_on=data.updated_on,
-                )
-                session.add(obj_)
-                session.commit()
-                session.refresh(obj_)
-                return obj_
-            except Exception:
-                session.rollback()
+    async def init_db(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
 
+    @staticmethod
+    async def add_data(session, data):
+        async with session() as session:
+            async with session.begin():
+                try:
+                    obj_ = SpimexTradingResultsBase(
+                        exchange_product_id=data.exchange_product_id,
+                        exchange_product_name=data.exchange_product_name,
+                        oil_id=data.oil_id,
+                        delivery_basis_id=data.delivery_basis_id,
+                        delivery_basis_name=data.delivery_basis_name,
+                        delivery_type_id=data.delivery_type_id,
+                        volume=data.volume,
+                        total=data.total,
+                        count=data.count,
+                        date=data.date,
+                        created_on=data.created_on,
+                        updated_on=data.updated_on,
+                    )
+                    session.add(obj_)
+                    session.commit()
+                    session.refresh(obj_)
+                    return obj_
+                except Exception as e:
+                    logging.warning(f"Ошибка при записи в БД - {e} - rollback")
+                    session.rollback()
